bc202374ffdb32cc9a1f19232093d632bf65ab17
Damiandegroot <146419969+Damiandegroot@users.noreply.github.com>
2026-03-16 10:08:51 +0100

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-window.RED_SYNC_CONFIG = {
-  supabaseUrl: "https://uyiegbxaxeuzzntaxwoq.supabase.co",
-  supabaseAnonKey: "sb_publishable_6kmngfWNfqGM1dhry7UHZQ_PkOpBSoX",
-  supabaseRedirectUrl: "https://dil-v3.vercel.app",
-};
